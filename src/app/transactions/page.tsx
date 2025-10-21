3eedfb4897d8cf5c1c98f8f48c78b05940901cb7
sainikhil kilani <kilanisainikhil@gmail.com>
2025-10-21 16:46:14 +0000

    Try fixing this error: `Runtime Error: Error: DialogTrigger is not defin

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -50,6 +50,7 @@ import {
   DialogTitle,
   DialogFooter,
   DialogClose,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
